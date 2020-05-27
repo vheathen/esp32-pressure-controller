7c25885988848339727de5326f5b41e973fd2175
heathen <v.heathen@gmail.com>
2020-05-27 18:58:30 +0500

    PressureSensors: artifacts removed

--- main/pressure_sensors.c
+++ main/pressure_sensors.c
@@ -65,47 +65,6 @@ int32_t pressures[CHAN_COUNT] = {PRESSURE_SENSOR_ABSENT};
 
 static TaskHandle_t uiTaskHandle;
 
-#if 0
-static void check_efuse(void)
-{
-    //Check TP is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK)
-    {
-        printf("eFuse Two Point: Supported\n");
-    }
-    else
-    {
-        printf("eFuse Two Point: NOT supported\n");
-    }
-
-    //Check Vref is burned into eFuse
-    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK)
-    {
-        printf("eFuse Vref: Supported\n");
-    }
-    else
-    {
-        printf("eFuse Vref: NOT supported\n");
-    }
-}
-
-static void print_char_val_type(esp_adc_cal_value_t val_type)
-{
-    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
-    {
-        printf("Characterized using Two Point Value\n");
-    }
-    else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
-    {
-        printf("Characterized using eFuse Vref\n");
-    }
-    else
-    {
-        printf("Characterized using Default Vref\n");
-    }
-}
-#endif
-
 uint32_t
 measure_absolute_voltage(adc_channel_t channel)
 {
